e9a64e3b2ee56fe40532de8197048a7edafca412
ipsumium <29902158+ipsumium@users.noreply.github.com>
2020-06-26 16:55:23 +0300

    Update messages.ts
    updated Romanian translations

--- web/src/translations/messages.ts
+++ web/src/translations/messages.ts
@@ -2,48 +2,48 @@
 export const MESSAGES = `
 ---
 id: "Unknown"
-en: "Unknown"
-ro: "Necunoscut"
+en: "Unknown. Please check if the card is inserted correctly"
+ro: "Necunoscut. Verificati daca cartela a fost introdusa corect."
 ---
 id: "Uploading a file on FTP"
 en: "Uploading a file on FTP"
-ro: "Uploading a file on FTP"
+ro: "Upload fisier in platforma"
 ---
 id: "FTP upload failure"
 en: "FTP upload failure"
-ro: "FTP upload failure"
+ro: "Eroare Upload FTP"
 ---
 id: "Completed. Take your card."
 en: "Completed. Take your card."
-ro: "Completed. Take your card."
+ro: "Terminat. Retrageti cardul"
 ---
 id: "Saving card data into a file"
 en: "Saving card data into a file"
-ro: "Saving card data into a file"
+ro: "Salvare informatii cartela"
 ---
 id: "Save file failure"
 en: "Save file failure"
-ro: "Save file failure"
+ro: "Eroare salvare fisier"
 ---
 id: "Parsing Card Data"
 en: "Parsing Card Data"
-ro: "Parsing Card Data"
+ro: "Citire informatii cartela"
 ---
 id: "Checking card readers"
 en: "Checking card readers"
-ro: "Checking card readers"
+ro: "Verificare cititor cartele"
 ---
 id: "Waiting a card"
-en: "Waiting a card"
-ro: "Waiting a card"
+en: "Insert tacho card"
+ro: "Introduceti cartela tahografica"
 ---
 id: "Reading the card"
 en: "Reading the card"
-ro: "Reading the card"
+ro: "Citire card"
 ---
 id: "Read card error: %1"
 en: "Read card error: %1"
-ro: "Read card error: %1
+ro: "Eroare citire cartela: %1
 ---
 # END OF FILE
 `
